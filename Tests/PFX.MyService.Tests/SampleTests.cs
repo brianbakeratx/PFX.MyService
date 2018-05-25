fff3c2e285d54458b1350167c99d2766a3de20f8
Brian Baker <brian.baker@mediware.com>
2018-05-25 10:32:17 -0500

    Initial import

---
+++ Tests/PFX.MyService.Tests/SampleTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace PFX.MyService.Tests
+{
+    [TestFixture]
+    public class SampleTests
+    {
+        [SetUp]
+        public void SetUpTests()
+        {
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void AlwaysTrue()
+        {
+            Assert.True(true);
+
+            var sample = true;
+            sample.Should().BeTrue();
+        }
+    }
+}
